c36d7cedebe3fd3e51f31dd6096205b161b127e8
mankamolnar <markmanomolnar@gmail.com>
2020-10-14 23:15:29 +0200

    Adding sidebutton component for left menu

---
+++ src/components/main-page/SideButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './main-page.css';
+import Optional from 'optional-js';
+
+export default class SideButton extends React.Component {
+  render() {
+    let isCommercial = Optional.ofNullable(this.props.isCommercial);
+    let borderColor = isCommercial.isPresent() && isCommercial.get()
+      ? "ffc108"
+      : "6d9639";
+
+    return (
+      <button
+          type="button"
+          className="btn btn-secondary rounded-0 bg-dark border-bottom-0 border-top-0 text-uppercase"
+          style={{"border-left": "5px solid #" + borderColor}}>
+
+        <h3>{this.props.children}</h3>
+      </button>
+    );
+  }
+}
