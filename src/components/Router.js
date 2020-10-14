7d5a96e02f518eb154fafd2efcb2f1901eadf330
mankamolnar <markmanomolnar@gmail.com>
2020-10-14 23:10:59 +0200

    clean up - delete unnecessary moduls

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,10 +5,6 @@ import {
   Route
 } from 'react-router-dom';
 import MainPage from './main-page/MainPage';
-import Teachers from './teachers/Teachers';
-import Courses from './courses/Courses';
-import ContactUs from './contact-us/ContactUs';
-import PrivateLesson from './private-lesson/PrivateLesson';
 import c404 from './404/404';
 import ScrollToTop from './common/scroll-to-top/ScrollToTop';
 import './common/bootstrap.min.css';
@@ -20,10 +16,6 @@ function Router() {
       <ScrollToTop>
         <Switch>
           <Route path={routes.mainPage} component={MainPage} exact />
-          <Route path={routes.courses} component={Courses} />
-          <Route path={routes.teachers} component={Teachers} />
-          <Route path={routes.privateLessons} component={PrivateLesson} />
-          <Route path={routes.contacts} component={ContactUs} />
           <Route path="*" component={c404} />
         </Switch>
       </ScrollToTop>
