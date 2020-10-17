a49c067bdae45f64a9054ed2ff235eb65f05b7b2
mankamolnar <markmanomolnar@gmail.com>
2020-10-17 20:45:23 +0200

    Adding auth functionality to Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,37 +1,69 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { connect } from "react-redux";
+import * as LoginActions from './../../redux/actions/loginActions';
 import Menu from '../common/menu/Menu';
 import Footer from '../common/footer/Footer';
 import Facebook from '../common/facebook/Facebook';
 
-export default class Login extends React.Component {
+
+class Login extends React.Component {
   constructor() {
     super();
-    this.doLogin.bind(this);
+    this.doLogin = this.doLogin.bind(this);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  onUsernameChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  onPasswordChange(event) {
+    this.setState({password: event.target.value});
   }
 
   doLogin() {
-    alert("Működöm");
+    this.props._fetchToken(
+      this.state.username,
+      this.state.password
+    );
   }
 
   render() {
+    let message = "";
+
+    if (this.props.token.fetched && this.props.token.error) {
+      message = <div className="alert alert-danger">Valami hiba történt! Próbáld újra!</div>;
+    } else if (this.props.token.fetched && !this.props.token.error) {
+      message = <Redirect to="/" />;
+    }
+
     return (
       <div>
         <Menu />
         <div className="row">
           <div className="col-md-1"></div>
           <div className="col-md-10">
+
+            {message}
+
             <h1>Bejelentkezés</h1>
             <form>
-              <div class="form-group">
-                <label for="username-input">Felhasználónév</label>
-                <input type="text" class="form-control" id="username-input" placeholder="Írj be felhasználónevet!" />
+              <div className="form-group">
+                <label htmlFor="username-input">Felhasználónév</label>
+                <input type="text" value={this.state.username} onChange={this.onUsernameChange} className="form-control" id="username-input" placeholder="Írj be felhasználónevet!" />
               </div>
-              <div class="form-group">
-                <label for="password-input">Jelszó</label>
-                <input type="password" class="form-control" id="password-input" placeholder="Írd be a jelszavad!" />
+              <div className="form-group">
+                <label htmlFor="password-input">Jelszó</label>
+                <input type="password" value={this.state.password} onChange={this.onPasswordChange} className="form-control" id="password-input" placeholder="Írd be a jelszavad!" />
               </div>
-              <button type="submit" class="btn btn-lg btn-success mb-3 mt-3" onClick={this.doLogin}>Bejelentkezés</button>
             </form>
+            <button className="btn btn-lg btn-success mb-3 mt-3" onClick={this.doLogin}>Bejelentkezés</button>
           </div>
           <div className="col-md-1"></div>
         </div>
@@ -42,3 +74,15 @@ export default class Login extends React.Component {
     );
   }
 }
+
+const selector = (store) => {
+  return {
+    token: store.token
+  }
+};
+
+const dispatcher = (dispatch) => ({
+  _fetchToken: (...args) => dispatch(LoginActions.fetchToken(...args))
+});
+
+export default connect(selector, dispatcher)(Login);
