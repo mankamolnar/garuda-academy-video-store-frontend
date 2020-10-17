cc34b54ca30fdedcf29d1ecf7b305af1cb35b0d0
mankamolnar <markmanomolnar@gmail.com>
2020-10-17 20:44:10 +0200

    Adding redux store provider to react app index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 import './components/common/common.css';
 
 serviceWorker.register();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
