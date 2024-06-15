6056df1adc387925558ab54b1b430ea5b16626d3
mankamolnar <markmanomolnar@gmail.com>
2024-06-15 11:56:19 +0200

    adding loading screen

---
+++ src/components/common/login-checker/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoadingScreen.css';
+
+export default function LoadingScreen() {
+  return (<>
+    <div className="d-flex justify-content-center mt-3"> 
+      <div className="spinner-grow text-success spinner-size-xl" role="status">
+      </div>
+    </div>
+    <div className="d-flex justify-content-center mt-3"> 
+      <h1>Automata bejelentkezés</h1>
+    </div>
+  </>);
+}
